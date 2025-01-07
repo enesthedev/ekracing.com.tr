6d52fb05f60f728772f9bde04ff0b1f636584c50
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2025-01-07 18:49:37 +0300

    feat: add next-auth v5

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,11 @@
 import chain from "@nimpl/middleware-chain";
 
-export default chain([]);
+import { auth as authMiddleware } from "@/lib/auth";
+import { Middleware } from "@nimpl/middleware-chain/dist/lib/types";
+
+export default chain([
+  [authMiddleware as unknown as Middleware, { include: /^\/private(\/.*)?$/ }],
+]);
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
