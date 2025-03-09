6e485ef36369404c66ca0efbe47321159962d05b
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2025-03-09 23:57:49 +0300

    refactor: disable auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,5 @@
 import chain from "@nimpl/middleware-chain";
 
-import { auth as authMiddleware } from "@/app/features/auth";
-import { Middleware } from "@nimpl/middleware-chain/dist/lib/types";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./app/features/i18n/routing";
 
@@ -9,7 +7,7 @@ const nextIntlMiddleware = createMiddleware(routing);
 
 export default chain([
   nextIntlMiddleware,
-  [authMiddleware as unknown as Middleware, { include: /^\/private(\/.*)?$/ }],
+  // [authMiddleware as unknown as Middleware, { include: /^\/private(\/.*)?$/ }],
 ]);
 
 export const config = {
